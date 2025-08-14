4bf10e5b8e6917d9c94b015f7300b53453d5d403
Sandesh Mandale <sandeshmandale05@gmail.com>
2025-08-14 23:04:31 +0530

    Rewrited

--- Recursion.java
+++ Recursion.java
@@ -1,22 +1,24 @@
-// import java.util.Scanner;
-// public class Recursion {
-//     public static void main(String args[]){
-//         System.out.println("Sandesh this said");
-//         Scanner sc=new Scanner(System.in);
-//         fact f1=new fact();
+import java.util.Scanner;
+public class Recursion {
+    public static void main(String args[]){
+        System.out.println("Sandesh this said");
+        Scanner sc=new Scanner(System.in);
+        fact f1=new fact();
         
-//         int result=f1.value(sc.nextInt());
-//         System.out.println(result);
-//     }
-// }
+        int result=f1.value(sc.nextInt());
+        System.out.println(result);
+    }
 
-// class fact{
-//     int value(int n){
-//         if(n==0){
-//             return 1;
-//         }
+    
+}
+
+class fact{
+    int value(int n){
+        if(n==0){
+            return 1;
+        }
       
-//             return n*value(n-1);
-        
-//     }
-// }
+            return n*value(n-1);
+         
+    }
+}
