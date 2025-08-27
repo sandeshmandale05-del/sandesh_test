5e0832e2184b39740bd764ae720710fb57cadc32
Sandesh Mandale <sandeshmandale05@gmail.com>
2025-08-27 22:39:44 +0530

    corrected code

--- Queen.java
+++ Queen.java
@@ -1,39 +1,62 @@
 public class Queen {
- 
+        public static boolean issafe(char board[][],int row,int col){
+    // verticaly up
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
 
-    public static void isRun(String str,int ans){
+            // vertical diagonal left
+            for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j--){
+                if(board[i][j]=='Q'){
+                    return false;
+                }
+            }
 
+            //diagonaly right up
+            for(int i=row-1, j=col+1; i>=0 && j<board.length; i-- ,j++){
+                if(board[i][j]=='Q'){
+                    return false;
+                }
+            } 
+        return true;
     }
     public static void nQueen(char board[][],int row){
         if(row==board.length){
-
+            printboard(board);
+            return;
         }
         for(int j=0;j<board.length;j++){
+            if(issafe(board,row,j)){
             board[row][j]='Q';
             nQueen(board,row+1);
             board[row][j]='x'; 
+            }
 
         }
     }
     public static void printboard(char board[][]){
-        for(int i=0;i<board.length;i++){
+        System.out.println("Chess board");
+        for(int i=0;i<board.length;i++){ 
+            
             for(int j=0;j<board.length;j++){
                 System.out.print(board[i][j]);
             }
             System.out.println();
-        }
+        } 
     }
     public static void main(String args[]){
         System.out.println("hii sandesh");
 
         
-        int n=2;
+        int n=4;
          char board[][]=new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 board[i][j]='X';
                 
-            }System.out.println();
+            }
         }
         nQueen(board,0);
 
